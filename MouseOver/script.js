56d8f30a870e452a8574021b9c9b1531cbf879ad
Pallavi Kumari <109738810+pallavik95@users.noreply.github.com>
2023-05-20 16:59:46 +0530

    Delete script.js

--- MouseOver/script.js
+++
@@ -1,6 +0,0 @@
-const doubleclik = document.getElementById("dclick");
-
-doubleclik.addEventListener("dblclick",()=>{
-//or , doubleclik.style.transform = "scale(1.3)";
-doubleclik.classList.toggle("large");
-});
